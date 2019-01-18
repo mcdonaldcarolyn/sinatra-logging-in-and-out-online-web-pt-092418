ca6c564a441ad703872f6bc15f56c603f3ef0f9f
Carolyn Whelpley <carolynwhelpley@hotmail.com>
2019-01-18 11:12:40 -0500

    Interim checkin

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,16 @@
 class Helpers
+
+  def current_user(session) 
+  session[:user_id] = User.id
+  end
+
+  def is_logged_in?(session)
+    @user = User.find_by(session[:user_id])
+    if
+    user_id != @user.id
+    puts "please Log on again"
+  else
+    @user
+  end
+  end
 end
